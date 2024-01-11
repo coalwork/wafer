0d2c0cda66df7cc0297338ed668f337afc50e34c
coalwork <cachedsid@gmail.com>
2024-01-11 03:53:54 -0600

    03:53 01/11/2024

--- src/api/register.ts
+++ src/api/register.ts
@@ -1,11 +1,28 @@
 import { default as express, RequestHandler } from 'express';
+import bcrypt from 'bcrypt';
 
+import User from '../schemas/User';
 import validator from './user-validator';
 
 const router = express.Router();
 
-router.post('/register', (req, res) => {
-  const { value, error } = validator(req.body);
+router.post('/register', async (req, res) => {
+  const { error: invalid } = validator(req.body);
+
+  if (invalid) 
+    return res.json({
+      errors: invalid.details.map(detail => detail.message)
+    });
+
+  try {
+    await User.create({
+      name: req.body.username,
+
+    });
+  } catch(error) {
+    if (error) return res.status(500).end();
+  }
+
   res.end();
 });
 
--- src/api/user-validator.ts
+++ src/api/user-validator.ts
@@ -1,28 +1,42 @@
-import { default as Joi, CustomValidator } from 'joi';
+import { default as Joi, ExternalValidationFunction } from 'joi';
 
 import User from '../schemas/User';
 
 export const userNameRegex = /^[a-zA-Z0-9-_() ]+$/;
 
 export const schema = Joi.object({
-  name: Joi.string()
+  username: Joi.string()
   .required()
+  .$
   .min(3)
   .max(20)
-  .pattern(userNameRegex),
+  .message('usernames must be 3 to 20 characters in length')
+  .pattern(userNameRegex)
+  .messages({
+    'string.empty': 'a username must be provided',
+    'string.pattern.base': 'usernames must consist of only letters, numbers, underscores (_), dashes (-), parentheses, or spaces'
+  }),
 
-  pass: Joi.string()
+  password: Joi.string()
   .required()
+  .$
   .min(6)
   .max(30)
-  .pattern(/[!-~]/),
+  .message('passwords must be 6 to 30 characters in length')
+  .pattern(/[!-~]/)
+  .messages({
+    'string.empty': 'a password must be provided',
+    'string.pattern.base': 'passwords may only contain letters, numbers, and punctuation (ASCII codes 33-126)'
+  }),
 
   email: Joi.string()
   .email()
+  .message('emails must be valid')
 });
 
-export const uniqueName: CustomValidator = (value, helpers) => {
-
+export const uniqueName: ExternalValidationFunction = name => {
+  if (!User.exists({ name })) throw Error('non unique username');
+  return;
 }
 
 export default function validator(user: object) {
